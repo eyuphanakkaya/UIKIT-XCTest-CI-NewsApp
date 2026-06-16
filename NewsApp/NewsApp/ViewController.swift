e1fdfc54c58be2f7802075200f83329ed6b0b670
eyuphanakkaya <akkayaeyupp@gmail.com>
2026-06-16 16:33:17 +0300

    Initial commit

---
+++ NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Eyüphan Akkaya on 16.06.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
