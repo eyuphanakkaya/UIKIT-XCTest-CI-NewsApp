a549a25cc070f2ae3b8952cff70a8cee672be0ae
eyuphanakkaya <akkayaeyupp@gmail.com>
2026-06-16 18:02:09 +0300

    create NewsModel

---
+++ NewsApp/NewsApp/Home/NewsModel.swift
@@ -0,0 +1,17 @@
+//
+//  NewsModel.swift
+//  NewsApp
+//
+//  Created by Eyüphan Akkaya on 16.06.2026.
+//
+
+import Foundation
+
+struct NewsModel: Equatable {
+    let id: String
+    let title: String
+    let imageURL: String
+    let creator: String
+    let pubDate: String
+    let description: String
+}
