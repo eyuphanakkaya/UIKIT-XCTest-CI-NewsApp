c74d6a5034bc5204190268279c86d48ba212d549
eyuphanakkaya <akkayaeyupp@gmail.com>
2026-06-19 18:19:47 +0300

    Create NewsCellViewModel.swift

---
+++ NewsApp/NewsApp/Features/Home/Cells/NewsCellViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  NewsCellViewModel.swift
+//  NewsApp
+//
+//  Created by Eyüphan Akkaya on 19.06.2026.
+//
+
+import Foundation
+
+struct NewsCellViewModel {
+    let title: String
+    let description: String?
+    let creator: String?
+    let date: String
+    let imageURL: String?
+    let isBookmarked: Bool
+}
