3b52cb6c623d86235df6a114beaa39239c41c145
eyuphanakkaya <akkayaeyupp@gmail.com>
2026-06-24 18:37:44 +0300

    Add UserDefaultsReadingListStore tests

---
+++ NewsApp/NewsTests/UserDefaults Reading List Store Tests/UserDefaultsReadingListStoreTests.swift
@@ -0,0 +1,39 @@
+//
+//  UserDefaultsReadingListStoreTests.swift
+//  NewsTests
+//
+//  Created by Eyüphan Akkaya on 24.06.2026.
+//
+
+import XCTest
+import NewsApp
+
+final class UserDefaultsReadingListStoreTests: XCTestCase {
+    
+    
+    func test_insert_deliverInsertedItem() async throws {
+        let sut = makeSUT()
+        
+        let item = await NewsModel(
+            id: "1",
+            title: "A title",
+            imageURL: "https://image.com/image.jpg",
+            creator: ["John"],
+            pubDate: "2026-06-22",
+            description: "Description")
+        
+        try await sut.insert(item)
+        
+        let result = try await sut.retrieve()
+        
+        XCTAssertEqual(result, [item])
+    }
+    
+    // MARK: - Helpers
+    private func makeSUT() -> UserDefaultsReadingListStore {
+         let sut = UserDefaultsReadingListStore(
+            userDefaults: UserDefaults(suiteName: UUID().uuidString)!
+        )
+        return sut
+    }
+}
